20f3a034facff79ef955a589e8e422f76f6edfe3
Remi <remigiusz.orlowski@gmail.com>
2017-04-19 21:00:38 +0200

    Simple cleanup

--- users.js
+++ users.js
@@ -1,22 +1,19 @@
-const express = require('express')
-const router = express.Router()
+const express = require('express');
 const uuid = require('uuid');
-const passport = require('passport')
+const passport = require('passport');
 const fsp = require('fs-promise');
 const path = require('path');
 const BasicStrategy = require('passport-http').BasicStrategy;
+
+const router = express.Router();
 
 const usersDir = './users';
 
 passport.use(new BasicStrategy(
   (userid, password, done) => {
-
-      if (err) { return done(err); }
-      if (!user) { return done(null, false); }
-      if (!user.verifyPassword(password)) { return done(null, false); }
-      return done(null, user);
-    }
-));
+    // check users files contents
+    // verify if we have user there...
+  }));
 
 const userSessions = [];
 
@@ -27,86 +24,92 @@ router.post('/register', async (req, res) => {
     const session = uuid.v4();
     const writeRes = await fsp.writeFile(path.join(usersDir, `${session}.json`),
         // TODO: MD5/SHA for password whatever!
-        JSON.stringify({name: user.name, email: user.email, luckyNumber: user.luckyNumber, password: user.password })
-    );
+        JSON.stringify({
+          name: user.name,
+          email: user.email,
+          luckyNumber: user.luckyNumber,
+          password: user.password,
+        }));
     // TODO: check unique!
-    userSessions.push({ email: user.email, name: user.name, file: session});
+    userSessions.push({ email: user.email, name: user.name, file: session });
     console.log('userSessions: ', userSessions);
-    res.json({ message: "User registered" });
+    res.json({ message: 'User registered' });
   } else {
-      res.status(400).end();
+    res.status(400).end();
   }
 });
 
 router.post('/login', async (req, res) => {
-    const login = req.body;
-    if (login.email && login.password && userSessions) {
-        const userSession = userSessions.find(s => (s.email === login.email));
-        if (userSession) {
-            let userData;
-            try {
-                userData = JSON.parse(await fsp.readFile(path.join(usersDir, `${userSession.file}.json`)));
-                if (userData && userData.password && userData.password === login.password) {
-                    const sessionId = uuid.v4();
-                    userSession.session = sessionId;
-                    return res.status(200).json({message: "Logged in", session: sessionId });
-                }
-            } catch (err) {
-                console.log(err)
-                return res.status(400).end();
-            }
-        } else {
-            console.log('user not found');
-            console.log('userSessions: ', userSessions);
+  const login = req.body;
+  if (login.email && login.password && userSessions) {
+    const userSession = userSessions.find(s => (s.email === login.email));
+    if (userSession) {
+      let userData;
+      try {
+        userData = JSON.parse(await fsp.readFile(path.join(usersDir, `${userSession.file}.json`)));
+        if (userData && userData.password && userData.password === login.password) {
+          const sessionId = uuid.v4();
+          userSession.session = sessionId;
+          return res.status(200).json({message: "Logged in", session: sessionId });
         }
+      } catch (err) {
+        console.log(err)
+        return res.status(400).end();
+      }
+    } else {
+      console.log('user not found');
+      console.log('userSessions: ', userSessions);
     }
-    return res.status(403).end();
+  }
+  return res.status(403).end();
 });
 
 
 
 router.post('/search', async (req, res) => {
-    const auth = req.headers['x-authorization'];
-    const query = req.body.query;
-    if (!query) {
-        return res.status(400).end();
+  const auth = req.headers['x-authorization'];
+  const query = req.body.query;
+  if (!query) {
+    return res.status(400).end();
+  }
+  if (userSessions) {
+    const userSessionIndex = userSessions.findIndex(s => s.session === auth);
+    if (userSessionIndex === -1) {
+      return res.status(403).end();
     }
-    if (userSessions) {
-        const userSessionIndex = userSessions.findIndex(s => s.session === auth);
-        if (userSessionIndex === -1) {
-            return res.status(403).end();
-        }
+  }
+  const result = userSessions.filter((s) => {
+    if ((s.email && s.email.includes(query)) || (s.name && s.name.includes(query))) {
+      return true;
     }
-    const result = userSessions.filter((s) => {
-        if ((s.email && s.email.includes(query)) || (s.name && s.name.includes(query))) {
-            return true;
-        }
-    }).map((s) => ({
-        name: s.name,
-        email: s.email,
-        uuid: s.file,
-        points: 0,
-    }))
-    return res.json(result);
+    return false;
+  }).map(s => ({
+    name: s.name,
+    email: s.email,
+    uuid: s.file,
+    points: 0,
+  }));
+  return res.json(result);
 });
 
 
 router.put('/logout', async (req, res) => {
-    const auth = req.headers['x-authorization'];
-    if (userSessions) {
-        const userSessionIndex = userSessions.findIndex(s => s.session === auth);
-        if (userSessionIndex >= 0 && userSessions[userSessionIndex].email) {
-            delete userSessions[userSessionIndex];
-            return res.status(200).end();
-        }
-        else {
-            console.log('user not found');
-            console.log('userSessions: ', userSessions);            
-        }
+  const auth = req.headers['x-authorization'];
+  if (userSessions) {
+    const userSessionIndex = userSessions.findIndex(s => s.session === auth);
+    if (userSessionIndex >= 0 && userSessions[userSessionIndex].email) {
+      delete userSessions[userSessionIndex];
+      return res.status(200).end();
     }
-    return res.status(404).end();
+    else {
+      console.log('user not found');
+      console.log('userSessions: ', userSessions);            
+    }
+  }
+  return res.status(404).end();
 });
 
 //   passport.authenticate('basic', { session: false }),
 
-module.exports = router
+module.exports = router;
+
